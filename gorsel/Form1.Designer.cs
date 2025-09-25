614716a0f4fb978d9089f8305b6be0df7adf1eea
Arda Tunca <ardatunca@marun.edu.tr>
2025-09-25 15:44:09 +0300

    Add project files.

---
+++ gorsel/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace gorsel
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            button1 = new Button();
+            timer1 = new System.Windows.Forms.Timer(components);
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold);
+            label1.ForeColor = SystemColors.ButtonFace;
+            label1.Location = new Point(65, 84);
+            label1.Name = "label1";
+            label1.Size = new Size(39, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Saat";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold);
+            label2.ForeColor = SystemColors.ButtonFace;
+            label2.Location = new Point(151, 84);
+            label2.Name = "label2";
+            label2.Size = new Size(56, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Dakika";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold);
+            label3.ForeColor = SystemColors.ButtonFace;
+            label3.Location = new Point(239, 84);
+            label3.Name = "label3";
+            label3.Size = new Size(54, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Saniye";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold);
+            label4.ForeColor = SystemColors.ButtonFace;
+            label4.Location = new Point(328, 84);
+            label4.Name = "label4";
+            label4.Size = new Size(48, 20);
+            label4.TabIndex = 3;
+            label4.Text = "Salise";
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveBorder;
+            button1.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            button1.Location = new Point(440, 80);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 81);
+            button1.TabIndex = 8;
+            button1.Text = "başla";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold);
+            label5.ForeColor = SystemColors.ButtonFace;
+            label5.Location = new Point(328, 146);
+            label5.Name = "label5";
+            label5.Size = new Size(48, 20);
+            label5.TabIndex = 12;
+            label5.Text = "Salise";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold);
+            label6.ForeColor = SystemColors.ButtonFace;
+            label6.Location = new Point(239, 146);
+            label6.Name = "label6";
+            label6.Size = new Size(54, 20);
+            label6.TabIndex = 11;
+            label6.Text = "Saniye";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold);
+            label7.ForeColor = SystemColors.ButtonFace;
+            label7.Location = new Point(151, 146);
+            label7.Name = "label7";
+            label7.Size = new Size(56, 20);
+            label7.TabIndex = 10;
+            label7.Text = "Dakika";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold);
+            label8.ForeColor = SystemColors.ButtonFace;
+            label8.Location = new Point(65, 146);
+            label8.Name = "label8";
+            label8.Size = new Size(39, 20);
+            label8.TabIndex = 9;
+            label8.Text = "Saat";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 192, 128);
+            ClientSize = new Size(767, 349);
+            Controls.Add(label5);
+            Controls.Add(label6);
+            Controls.Add(label7);
+            Controls.Add(label8);
+            Controls.Add(button1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+    }
+}
---
+++ gorsel/Form1.cs
@@ -0,0 +1,121 @@
+namespace gorsel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool calisti = false;
+
+        int salise = 0;
+        int saniye = DateTime.Now.Second - 3;
+        int dakika = DateTime.Now.Minute;
+        int saat = DateTime.Now.Hour;
+
+        int sistemsaat = DateTime.Now.Hour;
+        int sistemdakika = DateTime.Now.Minute;
+        int sistemsaniye = DateTime.Now.Second;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label3.Text = saniye.ToString();
+            label2.Text = dakika.ToString();
+            label1.Text = saat.ToString();
+            label4.Text = salise.ToString();
+
+            timer1.Interval = 10;
+            timer1.Start();
+            button1.Enabled = false;
+
+            if (button1.BackColor == Color.Green)
+            {
+                Environment.Exit(0);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (calisti == false)
+            {
+                salise++;
+                if (salise == 100)
+                {
+                    saniye++;
+                    salise = 0;
+                }
+
+                if (saniye == 60)
+                {
+                    dakika++;
+                    saniye = 0;
+                }
+
+                if (dakika == 60)
+                {
+                    saat++;
+                    dakika = 0;
+                }
+
+                if (saat == 24)
+                {
+                    saat = 0;
+                }
+
+                label3.Text = saniye.ToString();
+                label2.Text = dakika.ToString();
+                label1.Text = saat.ToString();
+                label4.Text = salise.ToString();
+            }
+
+            if (calisti == true)
+            {
+                
+
+                button1.Enabled = true;
+                button1.BackColor = Color.Green;
+
+                label5.Text = salise.ToString();
+                label6.Text = saniye.ToString();
+                label7.Text = dakika.ToString();
+                label8.Text = saat.ToString();
+
+                if (salise == 0)
+                {
+                    salise = 100;
+                    saniye--;
+                }
+
+                salise--;
+
+                if (saniye == 0)
+                {
+                    dakika--;
+                    saniye = 60;
+                }
+
+                if (dakika == 00)
+                {
+                    saat--;
+                    dakika = 60;
+                }
+
+                if (saat == 00)
+                {
+                    saat = 24;
+                }
+            }
+            if (saat == sistemsaat && dakika == sistemdakika && saniye == sistemsaniye && calisti == false)
+            {
+                timer1.Stop();
+                calisti = true;
+                button1.Enabled = true;
+            }
+        }
+    }
+}
